1d58918ca20b1cd7ee09f768447ad3bb8c909b13
Athul <engineer.athul@gmail.com>
2020-11-10 23:13:01 +0530

    build ready

---
+++ precache-manifest.1b36bef19185a984edec2c8d0ff026b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d42adcbe44caf6e4ca9911d76c5cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a250ca3dabd16dcd4000",
+    "url": "/static/css/main.ec75d18e.chunk.css"
+  },
+  {
+    "revision": "19a02f0edfd296cac57c",
+    "url": "/static/js/2.0c67da29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c67da29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a250ca3dabd16dcd4000",
+    "url": "/static/js/main.1ab6f2d0.chunk.js"
+  },
+  {
+    "revision": "5f8f0ca89b8f7bf10db3",
+    "url": "/static/js/runtime-main.d5ba2b5d.js"
+  }
+]);
